4fd76b608e8a58db2502668c7deee29ce0ced895
magdalenemensah <magdaleneafotey@yahoo.com>
2023-07-03 22:24:39 -0500

    java files comitted:venkat

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+class Arithmetic
+{
+public static void main(String args[])
+{
+int i=11;
+int j=12;
+
+int sum=i+j;
+int sub=i-j;
+
+System.out.println("the sum  is:"+sum);
+System.out.println("the sub is::"+sub);
+}
+}
+
